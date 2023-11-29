8523d0116973c1047375f64495d14f17ed931b15
Thanh Nguyen <btnguyen2k@gmail.com>
2023-11-29 14:23:41 +1100

    BREAKING: Refactor to move sub packages to separated repos

---
+++ module.go
@@ -0,0 +1,9 @@
+// Package prom provides helpers to manage shared connections in Go.
+package prom
+
+const (
+	// Version holds the semantic version number of this module.
+	Version = "0.4.1"
+)
+
+// This file contains module's metadata only, which is package level documentation and module Version string.
