b95fda18d506f46ea3337ab101f9d7d39286fe0d
Thanh Nguyen <btnguyen2k@gmail.com>
2023-12-03 00:28:27 +1100

    CI: refactor tests, SQLite

--- sql_test/sql_test.go
+++ sql_test/sql_test.go
@@ -11,14 +11,6 @@ import (
 	"strings"
 	"testing"
 	"time"
-
-	//_ "github.com/btnguyen2k/gocosmos"
-	//_ "github.com/denisenkom/go-mssqldb"
-	//_ "github.com/go-sql-driver/mysql"
-	//_ "github.com/godror/godror"
-	_ "github.com/jackc/pgx/v5/stdlib"
-	_ "github.com/mattn/go-sqlite3"
-	_ "modernc.org/sqlite"
 )
 
 type _testFailedWithMsgFunc func(msg string)
